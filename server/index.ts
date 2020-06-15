1038c11e5b65d93dc5d8a17099a99a2b748164e4
uday1331 <udayjain3112@gmail.com>
2020-06-15 11:49:58 +0800

    setup basic express server

---
+++ server/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3001;
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send(req.headers);
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
