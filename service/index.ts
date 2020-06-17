2062aa464a1d469280bf6bd3d73a71dc98d77d9b
uday1331 <udayjain3112@gmail.com>
2020-06-17 10:15:07 +0800

    working oath2 setup with jwt

--- service/index.ts
+++ service/index.ts
@@ -4,9 +4,9 @@ const app = express();
 
 const PORT = 3001;
 
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send({ ...req.headers, check: "test" });
-});
+app.get("/", (req: express.Request, res: express.Response) =>
+  res.send(req.headers)
+);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
